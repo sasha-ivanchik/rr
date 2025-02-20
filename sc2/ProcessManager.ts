f126c0c3de6400347364773944fc49be84631fe0
alexivanchyk <infosash88@gmail.com>
2025-02-20 12:33:11 -0600

    ui2

--- sc2/ProcessManager.ts
+++ sc2/ProcessManager.ts
@@ -1,4 +1,7 @@
 import { execSync } from 'child_process';
+import { promisify } from 'util';
+import { createConnection, Socket } from 'net';
+
 
 class ProcessManager {
     // Kill a process by name
@@ -20,12 +23,57 @@ class ProcessManager {
         }
     }
 
+    
     // Wait for a process to start
-    static async waitForProcess(processName: string, waitTime: number = 15_000, maxAttempts: number = 5): Promise<void> {
-        for (let i = 0; i < maxAttempts; i++) {
-            await new Promise((resolve) => setTimeout(resolve, waitTime));
-            if (ProcessManager.isProcessRunning(processName)) break;
+    async waitForProcess(processName: string, waitTime: number = 15_000, maxAttempts: number = 5): Promise<void> {
+        const execAsync = promisify(exec);
+        let attempts: number = 0;
+        while (attempts < maxAttempts) {
+            try {
+                const {stdout} = execAsync(`tasklist /FI "IMAGENAME eq ${processName}"`);
+                if (stdout.includes(processName)) {
+                    // TODO check port!
+                    return;
+                }
+            } catch(e) {
+                console.log(`error`);
+            }
+            attempts++;
+            console.log('waiting');
+            await new Promise((resolve) => setTimeout(resolve, waitTime))
         }
+    }
+
+    private async isPortAvailable(port: number, timeout: number): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const socket: Socket = createConnection({ port, timeout });
+        
+            const cleanup = () => {
+                socket.removeAllListeners();
+                socket.destroy();
+            };
+        
+            const timer = setTimeout(() => {
+                cleanup();
+                resolve(false);
+            }, timeout);
+        
+            socket
+                .once('connect', () => {
+                clearTimeout(timer);
+                cleanup();
+                resolve(true); // The port is available (there is a connection)
+                })
+                .once('error', (err: NodeJS.ErrnoException) => {
+                clearTimeout(timer);
+                cleanup();
+                if (err.code === 'ECONNREFUSED') {
+                    resolve(false); // The port is not listening
+                } else {
+                    reject(err); // Other errors
+                }
+            });
+        });
     }
 }
 
