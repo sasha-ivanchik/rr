63b0eb1e9a552ecce6be5b2b986f19f85618519c
alexivanchyk <infosash88@gmail.com>
2026-01-15 09:40:33 -0600

    dev104

---
+++ sc2/standalone/parent/stop.ts
@@ -0,0 +1,28 @@
+const cmd = 'powershell.exe';
+
+const psCmd = `
+$proc = Start-Process `
+  -FilePath "${openfinPath}" `
+  -ArgumentList @(
+    "--config=${confUrl}",
+    "--disable-gpu"
+  ) `
+  -WorkingDirectory "${process.cwd()}" `
+  -WindowStyle Normal `
+  -PassThru;
+
+Write-Output $proc.Id
+`;
+
+const args = [
+  '-NoProfile',
+  '-NonInteractive',
+  '-Command',
+  psCmd
+];
+
+const ps = spawn(cmd, args, {
+  stdio: ['ignore', 'pipe', 'pipe'],
+  windowsHide: false,        // 🔴 ОБЯЗАТЕЛЬНО
+  env: { ...process.env }    // 🔴 ОБЯЗАТЕЛЬНО
+});
